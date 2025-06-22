33452fc855e85bb206bfdecd2b12f38dd03b13fb
Александр Иванов <aleksandrivanov@Mac-mini.local>
2025-06-22 08:21:52 -0700

    Добавлен мячик

--- classes/Entity.lua
+++ classes/Entity.lua
@@ -11,6 +11,10 @@ function Entity:getRect()
     return self.pos.x, self.pos.y, self.w, self.h
 end
 
+function Entity:getCenter()
+    return vec(self.pos.x + self.w / 2, self.pos.y + self.h / 2)
+end
+
 function Entity:move(dx, dy)
 
 end
