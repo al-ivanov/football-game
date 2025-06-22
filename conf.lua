a27966fbfb90c9817a37e43dc24dee338d450ba3
Александр Иванов <aleksandrivanov@Mac-mini.local>
2025-06-22 05:20:56 -0700

    Init commit

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.console = true
+    t.width = 800
+    t.height = 450
+end
---
+++ main.lua
@@ -0,0 +1,19 @@
+lg = love.graphics
+lw = love.window
+lk = love.keyboard
+
+function love.load()
+    lw.setTitle('Telekinese')
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(k)
+
+end
