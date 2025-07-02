274ffc31ad4115e5b4434bc77a8747209715eb92
Александр Иванов <aleksandrivanov@Mac-mini.local>
2025-07-02 08:11:19 -0700

    Заменены звуки и имплементация ворот

--- classes/Ball.lua
+++ classes/Ball.lua
@@ -7,17 +7,26 @@ function Ball:init(cx, cy, sprite)
     self.isHold = 0
     self.auraColor = colors.white
     self.auraRad = 35
+    self.inGoal = false
 
     Entity.init(self, cx - sprite:getWidth() / 2, cy - sprite:getHeight() / 2, sprite:getWidth(), sprite:getHeight())
 
     -- collision
     world:add(self, self:getRect())
 
+    self.filter = function (item, other)
+        if other.id == 'goal' then
+            return 'cross'
+        end
+
+        return 'slide'
+    end
+
     self.filter = nil
 end
 
 function Ball:update(dt, p1, p2)
-    local actualX, actualY, cols, len = world:move(self, self.pos.x + self.velVec.x, self.pos.y + self.velVec.y)
+    local actualX, actualY, cols, len = world:move(self, self.pos.x + self.velVec.x, self.pos.y + self.velVec.y, self.filter)
     if self.isHold == 0 then
         if (p1:getCenter() - self:getCenter()):len() < telekinesisRadius or (p2:getCenter() - self:getCenter()):len() < telekinesisRadius then
             self.auraRad = 50
@@ -36,7 +45,8 @@ function Ball:update(dt, p1, p2)
         end
 
 
-        -- bounce on walls
+        -- collision
+        local notInGoal = true
         for i=1, len do
             local col = cols[i]
             local otherObj = col.other
@@ -47,18 +57,20 @@ function Ball:update(dt, p1, p2)
                 else 
                     self.velVec.x = self.velVec.x * - 1
                 end
-                if self.velVec:len() > 5 then
-                    exp3:play()
+                if self.velVec:len() > 10 then
+                    hit1:play()
                 end
             elseif otherObj.id == 'ball' then
-                --trade
-                --[[local tx, ty = otherObj.velVec.x, otherObj.velVec.y
-                otherObj.velVec.x, otherObj.velVec.y = self.velVec.x, self.velVec.y
-                self.velVec.x, self.velVec.y = tx, ty]]
+                -- bounce off ball
                 local sumVec = self.velVec + otherObj.velVec
                 self.velVec.x, self.velVec.y = sumVec.x * -0.5, sumVec.y * -0.5
                 otherObj.velVec.x, otherObj.velVec.y = sumVec.x * 0.8, sumVec.y * 0.8
+            elseif otherObj.id == 'goal' then
+                notInGoal = false
             end
+        end --for all collisions
+        if notInGoal then
+            self.inGoal = false
         end
     end -- if ball is active
 
@@ -68,7 +80,7 @@ end
 function Ball:draw()
     -- lg.draw(self.sprite, self.pos.x, self.pos.y)
     lg.setColor(self.auraColor[1], self.auraColor[2], self.auraColor[3], 0.7)
-    lg.circle('fill', self.pos.x + self.w / 2, self.pos.y + self.h / 2, self.auraRad)
+    lg.circle('fill', self.pos.x + self.w / 2, self.pos.y + self.h / 2, self.auraRad + lm.random(-1, 1))
 
     lg.setColor(colors.white)
     lg.draw(self.sprite, self.pos.x + self.w / 2, self.pos.y + self.h / 2, self.velVec:len() % (2 * math.pi), 1, 1, self.w / 2, self.h / 2)
--- states/gameScreen.lua
+++ states/gameScreen.lua
@@ -16,6 +16,13 @@ local walls = {
     Wall(400, 455, 800, 10),
 }
 
+local goals = {
+    Goal(50, 225, 10, 450, 1),
+    Goal(750, 225, 10, 450, 2)
+}
+
+local maxScore = 5
+
 local scores = {0, 0}
 
 local balls = {
@@ -29,22 +36,28 @@ local balls = {
 local p1 = Player(300, 225, spritesheet, plAnims, colors.aqua, step3)
 local p2 = Player(500, 325, spritesheet, plAnims, colors.orange, step4)
 p2.facing = -1
+local gameEnd = false
 
 function gameScreen:reset()
     screen:setShake(10)
-    scores[0], scores[1] = 0, 0
+    exp3:play()
+    scores[1], scores[2] = 0, 0
+    gameEnd = false
 end
 
 function gameScreen:enter()
     self.reset()
-    exp3:play()
 end
 
 function gameScreen:update(dt)
+    if scores[1] >= maxScore or scores[2] >= maxScore then
+        gameEnd = true
+    end
     local dx, dy = 0, 0
     screen:update(dt)
     
     -- p1 input
+    -- keyboard
     if (lk.isDown('w')) then dy = dy - 1 end
     if (lk.isDown('s')) then dy = dy + 1 end
     if (lk.isDown('a')) then dx = dx - 1 end
@@ -59,12 +72,19 @@ function gameScreen:update(dt)
     if (lk.isDown('down')) then dy = dy + 1 end
     if (lk.isDown('left')) then dx = dx - 1 end
     if (lk.isDown('right')) then dx = dx + 1 end
+
+    -- joystick
     
     p2:update(dt, dx, dy)
     
     -- update balls
     for i,ball in ipairs(balls) do
         ball:update(dt, p1, p2)
+    end
+
+    -- update goals
+    for i,goal in ipairs(goals) do
+        goal:update(dt, scores)
     end
 end
 
@@ -75,15 +95,29 @@ function gameScreen:draw()
 
         screen:apply()
         lg.draw(assets.sprites.field1, 0, 0)
-        
-        --draw players
-        p1:draw()
-        p2:draw()
-    
 
         -- draw balls
         for i,ball in ipairs(balls) do
             ball:draw()
+        end
+
+        if debug then
+            for i,goal in ipairs(goals) do
+                goal:draw()
+            end
+        end
+        
+        --draw players
+        p1:draw()
+        p2:draw()
+
+        -- draw scores
+        lg.setColor(colors.black)
+        lg.printf(scores[1] .. '/' .. maxScore .. '  ' .. scores[2] .. '/' .. maxScore ..'', 0, 50, gameW, 'center')
+        
+        lg.setColor(colors.white)
+        if scores[1] >= 10 then
+            lg.draw(assets.sprites.trophy, 330, 50)
         end
 
     end)
@@ -92,11 +126,14 @@ function gameScreen:draw()
 end
 
 function gameScreen:keypressed(k)
-    if (k == 'space') then
+    if k == 'r' then
+        self.reset()
+    end
+    if k == 'space' then
         p1:action(balls)
     end
     
-    if (k == 'return') then
+    if k == 'return' then
         p2:action(balls)
     end
 end
